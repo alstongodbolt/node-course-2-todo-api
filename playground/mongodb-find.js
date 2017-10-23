90b4180f23cea042ecebd4d77084ecc4abaffc95
Alston Godbolt <godbolta@onid.oregonstate.edu>
2017-10-22 21:29:12 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+const {MongoClient, ObjectId} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server')
+  }
+  console.log('Connected to MongoDB server')
+
+//   db.collection('Todos').find({
+//     _id: new ObjectId('59ed4f751fa0d9e58cc834ad')}).toArray().then((docs) => {
+//       console.log('Todos')
+//       console.log(JSON.stringify(docs, undefined, 2))
+//     }, (err) => {
+//       console.log('Unable to fetch todos', err)
+//     })
+
+//   db.collection('Todos').find().count().then((count) => {
+//     console.log(`Todos count: ${count}`)
+//   }, (err) => {
+//     console.log('Unable to fetch todos', err)
+//   })
+
+  db.collection('Users').find({name: 'Alston Godbolt'}).toArray().then((docs) => {
+    console.log('Users')
+    console.log(JSON.stringify(docs, undefined, 2))
+  }, (err) => {
+    console.log('Unable to fetch Users', err)
+  })
+
+  // db.close()
+})
